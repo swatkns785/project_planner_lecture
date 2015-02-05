bb9afa162fa465f5ea53dc75f8e4bbf46ef97887
Steven Watkins <steven.watkins78@gmail.com>
2015-02-04 19:45:09 -0500

    added save functions

--- backend/app/controllers/api/v1/projects_controller.rb
+++ backend/app/controllers/api/v1/projects_controller.rb
@@ -12,4 +12,22 @@ class Api::V1::ProjectsController < ApplicationController
    render json: @project
  end
 
+ def create
+   @project = Project.new(project_params)
+
+   if @project.save
+     render json: @project,
+      status: :created
+   else
+     render json: @project.errors,
+      status: :unprocessable_entity
+   end
+ end
+
+ private
+
+ def project_params
+   params.require(:project).permit(:name, :description)
+ end
+
 end
--- frontend/app/routes/projects/index.js
+++ frontend/app/routes/projects/index.js
@@ -3,5 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('project');
+  },
+
+  setupController: function(controller, model) {
+    controller.set('model', model);
+
+    var newProject = this.store.createRecord('project');
+    controller.set('newProject', newProject);
+  },
+
+  actions: {
+    createProject: function() {
+      var project = this.get('controller.newProject');
+
+      project.save();
+    }
   }
 });
